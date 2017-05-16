95bb92adb3769e8150c50bf20efdc6ab10e1c805
yuanfang1987 <176363909@qq.com>
2017-05-16 15:44:37 +0800

    update cleaner.go logic

--- src/oceanwing/eufy/robot/cleaner.go
+++ src/oceanwing/eufy/robot/cleaner.go
@@ -22,13 +22,15 @@ func NewRobotCleaner() *Cleaner {
 // Cleaner struct.
 type Cleaner struct {
 	mqttclient.MqttClient
-	Cindex   int
-	Command1 []byte
-	Command2 []byte
-	Command3 []byte
-	Command4 []byte
-	Command5 []byte
-	Command6 []byte
+	robotACK    []byte
+	CmdToServer chan []byte
+	Cindex      int
+	Command1    []byte
+	Command2    []byte
+	Command3    []byte
+	Command4    []byte
+	Command5    []byte
+	Command6    []byte
 }
 
 //RunRobotCleanerMqttService connect to mqtt and subscribeto topic. so cool.
@@ -40,12 +42,16 @@ func (r *Cleaner) RunRobotCleanerMqttService(clientid, username, pwd, broker, de
 	r.PubTopic = "DEVICE/T2103/" + devKey + "/PUH_MESSAGE"
 	r.SubTopic = "DEVICE/T2103/" + devKey + "/SUB_MESSAGE"
 	r.NeedCA = needCA
+	// 2017.05.16 added
+	r.robotACK = []byte{0xA5, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x5C, 0x00, 0x00, 0x01, 0x10, 0x09, 0xFF, 0xFF, 0x74, 0xFA}
 	// msgHandler
 	r.SubHandler = func(c MQTT.Client, msg MQTT.Message) {
-		r.ReceivedPayload = msg.Payload()
+		go r.handleInComingCMD(msg.Payload())
 	}
 	// connect to broker and subscribe to a topic.
 	r.MqttClient.ConnectToBroker()
+	// 2017.05.16 added. 作为一个publish消息的统一的出口
+	r.outgoingMsg()
 }
 
 // GetCommand hh.
@@ -71,12 +77,71 @@ func (r *Cleaner) getCommand() []byte {
 
 // SendRobotCleanerHeartBeat send a heart beat to broker.
 func (r *Cleaner) SendRobotCleanerHeartBeat() {
-	r.MqttClient.PublishMessage(r.getCommand())
+	//r.MqttClient.PublishMessage(r.getCommand())
+	r.CmdToServer <- r.robotACK
 	// update index.
 	r.Cindex++
 	if r.Cindex > 5 {
 		r.Cindex = 0
 	}
+}
+
+func (r *Cleaner) outgoingMsg() {
+	go func() {
+		for {
+			select {
+			case payload := <-r.CmdToServer:
+				r.MqttClient.PublishMessage(payload)
+			}
+		}
+	}()
+}
+
+func (r *Cleaner) handleInComingCMD(pl []byte) {
+	if len(pl) != 8 {
+		return
+	}
+	if pl[4] == 225 {
+		switch pl[5] {
+		case 0:
+			// pause
+			r.robotACK = []byte{0xA5, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x5C, 0x00, 0x00, 0x01, 0x10, 0x09, 0xFF, 0xFF, 0x74, 0xFA}
+		case 1:
+			// spot
+			r.robotACK = []byte{0xA5, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x5D, 0x00, 0x00, 0x00, 0x10, 0x08, 0xFF, 0xFF, 0x74, 0xFA}
+		case 2:
+			// auto
+			r.robotACK = []byte{0xA5, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x5D, 0x00, 0x00, 0x00, 0x10, 0x09, 0xFF, 0xFF, 0x76, 0xFA}
+		case 3:
+			// back to charge
+			r.robotACK = []byte{0xA5, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x5D, 0x01, 0x00, 0x01, 0x10, 0x0F, 0xFF, 0xFF, 0x7F, 0xFA}
+		case 4:
+			// edge
+			r.robotACK = []byte{0xA5, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x5D, 0x00, 0x00, 0x00, 0x10, 0x00, 0xFF, 0xFF, 0x6F, 0xFA}
+		case 5:
+			// small room
+			r.robotACK = []byte{0xA5, 0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x5C, 0x00, 0x00, 0x00, 0x10, 0x09, 0xFF, 0xFF, 0x78, 0xFA}
+		}
+	} else if pl[4] == 232 {
+		switch pl[5] {
+		case 0:
+			// daily
+			if r.robotACK[8] != 0 {
+				r.robotACK[8] = 0x00
+				r.robotACK[18] = r.robotACK[18] - 1
+			}
+		case 1:
+			// strong
+			if r.robotACK[8] != 1 {
+				r.robotACK[8] = 0x01
+				r.robotACK[18] = r.robotACK[18] + 1
+			}
+		}
+	} else if pl[4] == 236 {
+		return
+		// do nothing, just let the robot make some noise.
+	}
+	r.CmdToServer <- r.robotACK
 }
 
 // SendRequestLocalCodeCommand hh.
