13009d548f21e5d8edf985b6f1c150b17eec13e6
yuanfang1987 <176363909@qq.com>
2017-05-27 16:38:44 +0800

    haha

--- src/runamazon/main.go
+++ src/runamazon/main.go
@@ -13,5 +13,8 @@ func main() {
 	heads := make(map[string]string)
 	heads["User-Agent"] = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0"
 	heads["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+	heads["Accept-Encoding"] = "gzip, deflate, sdch, br"
+	heads["Accept-Language"] = "zh-CN,zh;q=0.8"
+	heads["Connection"] = "keep-alive"
 	crawler.RunVersion2("https://www.amazon.com/b?ie=UTF8&node=13727921011", heads)
 }
