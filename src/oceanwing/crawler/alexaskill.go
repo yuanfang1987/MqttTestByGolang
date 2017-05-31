cd584e212682fef0af486de053d896f4e400f155
yuanfang1987 <176363909@qq.com>
2017-05-31 15:31:05 +0800

    hehe

--- src/oceanwing/crawler/alexaskill.go
+++ src/oceanwing/crawler/alexaskill.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 	"time"
 
+	"io/ioutil"
+
 	"github.com/PuerkitoBio/goquery"
 	log "github.com/cihub/seelog"
 )
@@ -32,21 +34,21 @@ func newAlexaSkill(headerMap map[string]string) *AlexaSkill {
 func (a *AlexaSkill) addHeaders(req *http.Request) {
 	for key, value := range a.headers {
 		req.Header.Add(key, value)
-		//log.Debugf("add header, key: %s, value: %s", key, value)
+		log.Debugf("add header, key: %s, value: %s", key, value)
 	}
 }
 
 func (a *AlexaSkill) addCookies(req *http.Request) {
 	for _, c := range a.cookieList {
 		req.AddCookie(c)
-		//log.Debugf("add cookie: %s", c.String())
+		log.Debugf("add cookie: %s", c.String())
 	}
 }
 
-func (a *AlexaSkill) sendRequest(method, url string, needCookie bool) (*http.Response, error) {
+func (a *AlexaSkill) sendRequest(method, url string, needCookie bool) (*goquery.Document, error) {
 	//为了应对反爬虫，在每次发起请求之前，先暂停 X 秒钟, X 是一个随机数
 	pauseTime := commontool.RandInt64(2, 7)
-	log.Infof("Sleep %d seconds before request next URL: %s", pauseTime, url)
+	log.Debugf("Sleep %d seconds before request next URL: %s", pauseTime, url)
 	time.Sleep(time.Duration(pauseTime) * time.Second)
 	var resp *http.Response
 	var err error
@@ -59,7 +61,17 @@ func (a *AlexaSkill) sendRequest(method, url string, needCookie bool) (*http.Res
 	for i := 0; i < 3; i++ {
 		resp, err = a.client.Do(req)
 		if err == nil {
-			return resp, nil
+			defer resp.Body.Close()
+			// get cookies
+			a.cookieList = resp.Cookies()
+			bd, _ := ioutil.ReadAll(resp.Body)
+			log.Debugf("response code: %d", resp.StatusCode)
+			log.Debugf("get content: %s", string(bd))
+			doc, err := goquery.NewDocumentFromResponse(resp)
+			if err == nil {
+				return doc, nil
+			}
+			return nil, err
 		}
 		time.Sleep(time.Duration(commontool.RandInt64(2, 6)) * time.Second)
 		log.Warnf("request fail, try times: %d, err: %s", i, err)
@@ -70,96 +82,72 @@ func (a *AlexaSkill) sendRequest(method, url string, needCookie bool) (*http.Res
 
 // get category urls and store their names.
 func (a *AlexaSkill) getCategoryURLs(url string) {
-	resp, err := a.sendRequest("GET", url, false)
-	if err != nil {
-		log.Error("Stop programe")
-		os.Exit(1)
-	}
-	defer resp.Body.Close()
-	// get cookies
-	a.cookieList = resp.Cookies()
-	doc, err := goquery.NewDocumentFromResponse(resp)
+	doc, err := a.sendRequest("GET", url, false)
 	if err != nil {
 		log.Errorf("goquery parse doc from response fail: %s", err.Error())
 		os.Exit(1)
 	}
-
+	//debug find title
+	title := doc.Find("title").Text()
+	log.Infof("title is: %s", title)
 	li := doc.Find("div.categoryRefinementsSection").Find("ul").Find("li")
 	log.Infof("Found category URL number: %d", li.Length())
-	// if li.Length() == 0 {
-	// 	bd, _ := ioutil.ReadAll(resp.Body)
-	// 	log.Infof("response body content: %s", string(bd))
-	// 	os.Exit(1)
-	// }
 	myFunc := func(index int, sel *goquery.Selection) {
 		// get category urls.
 		href, bl := sel.Find("a").Attr("href")
 		if bl {
 			a.categoryURLs = append(a.categoryURLs, "https://www.amazon.com"+href)
-			log.Debugf("category index: %d, url: %s", index, href)
+			log.Infof("category index: %d, url: %s", index, href)
 		}
 		// get category name.
 		name := sel.Find("span").First().Text()
 		if name != "" {
 			a.categoryName = append(a.categoryName, name)
-			log.Debugf("category name: %s", name)
+			log.Infof("category name: %s", name)
 		}
 	}
 	li.Each(myFunc)
 }
 
 // 当进入某个category之后，取当前页面的16个item的URL
-func (a *AlexaSkill) getCurrentPageItems(resp *http.Response) ([]string, string) {
+func (a *AlexaSkill) getCurrentPageItems(doc *goquery.Document) ([]string, string) {
 	var allItemsURL []string
-	defer resp.Body.Close()
-	doc, err := goquery.NewDocumentFromResponse(resp)
-	if err != nil {
-		log.Errorf("goquery parse resp fail: %s", err.Error())
-		return []string{}, ""
-	}
-
 	li := doc.Find("div.s-item-container")
 	log.Infof("Found %d items on current page.", li.Length())
 	getItemURLFunc := func(index int, sel *goquery.Selection) {
 		url, bl := sel.Find("a").First().Attr("href")
 		if bl {
 			allItemsURL = append(allItemsURL, url)
-			log.Debugf("get item url: %s", url)
+			log.Infof("get item url: %s", url)
 		}
 	}
 	li.Each(getItemURLFunc)
 	// get next page url
 	nextURL := a.getNextPageURL(doc)
-	log.Debugf("get next page link is: %s", nextURL)
+	log.Infof("get next page link is: %s", nextURL)
 	return allItemsURL, nextURL
 }
 
 // 获取app的title, review star, review number, cmd 等数据
-func (a *AlexaSkill) getData(categoryName, url string, resp *http.Response) []string {
+func (a *AlexaSkill) getData(categoryName, url string, doc *goquery.Document) []string {
 	var contentList []string
-	defer resp.Body.Close()
-	doc, err := goquery.NewDocumentFromResponse(resp)
-	if err != nil {
-		log.Errorf("goquery parse resp fail: %s", err.Error())
-		return []string{}
-	}
 	// get cmd title
 	title := doc.Find("h1.a2s-title-content").Text()
 	title = strings.TrimSpace(title)
-	log.Debugf("App category is: %s", categoryName)
-	log.Debugf("App title is: %s", title)
-	log.Debugf("App url is: %s", url)
+	log.Infof("App category is: %s", categoryName)
+	log.Infof("App title is: %s", title)
+	log.Infof("App url is: %s", url)
 	// get review star
 	starStr, bl := doc.Find("div.a2s-pdd-reviews").Find("i").Attr("class")
 	if bl {
 		starStr = getReviewStar(starStr)
-		log.Debugf("App review star is: %s", starStr)
+		log.Infof("App review star is: %s", starStr)
 	}
 	// get review number
 	starNum := doc.Find("div.a2s-pdd-reviews").Find("span").Eq(1).Text()
 	// strings.Replace(starNum, " ", "", -1)
 	starNum = strings.TrimSpace(starNum)
-	log.Debugf("App review number is: %s", starNum)
+	log.Infof("App review number is: %s", starNum)
 
 	contentList = append(contentList, categoryName, title, url, starStr, starNum)
 	// get cmd span.a2s-utterance-text
@@ -171,7 +159,7 @@ func (a *AlexaSkill) getData(categoryName, url string, resp *http.Response) []st
 			cmdName = strings.TrimSpace(cmdName)
 			//去掉双引号
 			cmdName = strings.Replace(cmdName, `"`, "", -1)
-			log.Debugf("App's %d cmd is: %s", index, cmdName)
+			log.Infof("App's %d cmd is: %s", index, cmdName)
 			contentList = append(contentList, cmdName)
 		}
 	}
@@ -208,13 +196,13 @@ func (a *AlexaSkill) runEngineVersion2(cateIndex, pageIndex int) {
 	var content []string
 	var nextPageURL string
 	var flag bool
-	var resp *http.Response
+	var doc *goquery.Document
 	var err error
 	var currentPageIndex int
 	// i, categoryURL := range a.categoryURLs
 	for i := cateIndex; i < len(a.categoryURLs); i++ {
 		currentPageIndex = 1
-		resp, err = a.sendRequest("GET", a.categoryURLs[i], true)
+		doc, err = a.sendRequest("GET", a.categoryURLs[i], true)
 		if err != nil {
 			log.Errorf("Enter category fail: %s", a.categoryURLs[i])
 			continue
@@ -225,9 +213,9 @@ func (a *AlexaSkill) runEngineVersion2(cateIndex, pageIndex int) {
 
 		for flag {
 			// 取当前页的 16 个item的URL以及 Next Page 的URL
-			log.Debugf("Current page is Number.%d page.", currentPageIndex)
-			currentItemURLs, nextPageURL = a.getCurrentPageItems(resp)
-			log.Debugf("current item number: %d", len(currentItemURLs))
+			log.Infof("Current page is Number.%d page.", currentPageIndex)
+			currentItemURLs, nextPageURL = a.getCurrentPageItems(doc)
+			log.Infof("current item number: %d", len(currentItemURLs))
 			for _, itemURL := range currentItemURLs {
 				// 如果指定到具体的某一页才开始收集数据， 则忽略掉前面的
 				if pageIndex != 0 && currentPageIndex < pageIndex {
@@ -235,17 +223,17 @@ func (a *AlexaSkill) runEngineVersion2(cateIndex, pageIndex int) {
 				}
 				//
 				pageIndex = 0
-				resp, err = a.sendRequest("GET", itemURL, true)
+				doc, err = a.sendRequest("GET", itemURL, true)
 				if err != nil {
 					log.Errorf("Enter Item page: %s fail, err: %s", itemURL, err.Error())
 					continue
 				}
-				content = a.getData(cateName, itemURL, resp)
+				content = a.getData(cateName, itemURL, doc)
 				writeToResult(content)
 			}
 			// 进入下一页
 			if nextPageURL != "" {
-				resp, err = a.sendRequest("GET", nextPageURL, true)
+				doc, err = a.sendRequest("GET", nextPageURL, true)
 				if err != nil {
 					log.Errorf("Enter next page fail: %s, current page is: %d", err.Error(), currentPageIndex)
 				}
@@ -268,70 +256,4 @@ func RunVersion2(url string, heads map[string]string, cIndex, pIndex int) {
 	inst := newAlexaSkill(heads)
 	inst.getCategoryURLs(url)
 	inst.runEngineVersion2(cIndex, pIndex)
-}
-
-// RunByConcurrentMode hh.
-func RunByConcurrentMode(url string, heads map[string]string) {
-	//create result file
-	createNewFile("alexaData.csv")
-	defer closeFile()
-
-	firstInst := newAlexaSkill(heads)
-	firstInst.getCategoryURLs(url)
-	allCategoryURL := firstInst.categoryURLs
-	allCategoryName := firstInst.categoryName
-
-	for i, v := range allCategoryURL {
-		go func(urls, name string) {
-			inst := newAlexaSkill(heads)
-			inst.runEngineConcurrent(urls, name)
-		}(v, allCategoryName[i])
-	}
-
-	forever := make(chan struct{})
-	<-forever
-}
-
-func (a *AlexaSkill) runEngineConcurrent(categoryURL, categoryName string) {
-	var currentItemURLs []string
-	var content []string
-	var nextPageURL string
-	var flag bool
-	var resp *http.Response
-	var err error
-	var currentPageIndex int
-
-	resp, err = a.sendRequest("GET", categoryURL, false)
-	if err != nil {
-		log.Errorf("Enter category: %s fail: %s", categoryURL, err.Error())
-	}
-	log.Debugf("Enter category: %s success.", categoryName)
-
-	flag = true
-	currentPageIndex = 1
-	for flag {
-		// 取当前页的 16 个item的URL以及 Next Page 的URL
-		log.Debugf("Current page is Number.%d page.", currentPageIndex)
-		currentItemURLs, nextPageURL = a.getCurrentPageItems(resp)
-		log.Debugf("current item number: %d", len(currentItemURLs))
-		for _, itemURL := range currentItemURLs {
-			resp, err = a.sendRequest("GET", itemURL, false)
-			if err != nil {
-				log.Errorf("Enter Item page: %s fail, err: %s", itemURL, err.Error())
-				continue
-			}
-			content = a.getData(categoryName, itemURL, resp)
-			go writeToResult(content)
-		}
-		// 进入下一页
-		if nextPageURL != "" {
-			resp, err = a.sendRequest("GET", nextPageURL, false)
-			if err != nil {
-				log.Errorf("Enter next page fail: %s, current page is: %d", err.Error(), currentPageIndex)
-			}
-			currentPageIndex++
-		} else {
-			flag = false
-		}
-	}
 }
--- src/runamazon/main.go
+++ src/runamazon/main.go
@@ -13,14 +13,14 @@ func main() {
 	defer log.Flush()
 	specifiedCateIndex := flag.Int("CateIndex", 0, "category index")
 	specifiedPageIndex := flag.Int("PageIndex", 0, "page index")
-	logLevel := flag.String("logLevel", "debug", "log level")
+	logLevel := flag.String("logLevel", "info", "log level")
 	flag.Parse()
 	commontool.InitLogInstance(*logLevel)
 	heads := make(map[string]string)
 	heads["User-Agent"] = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:45.0) Gecko/20100101 Firefox/45.0"
 	heads["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
-	heads["Accept-Encoding"] = "gzip, deflate, sdch, br"
-	heads["Accept-Language"] = "zh-CN,zh;q=0.8"
-	heads["Connection"] = "keep-alive"
+	//heads["Accept-Encoding"] = "gzip, deflate, sdch, br"
+	//heads["Accept-Language"] = "zh-CN,zh;q=0.8"
+	//heads["Connection"] = "keep-alive"
 	crawler.RunVersion2("https://www.amazon.com/b?ie=UTF8&node=13727921011", heads, *specifiedCateIndex, *specifiedPageIndex)
 }
