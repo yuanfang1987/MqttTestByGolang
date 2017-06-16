6fb66c8f2f7d390134f9d3833e94a48753992169
yuanfang1987 <176363909@qq.com>
2017-06-16 17:00:06 +0800

    update basedevice.go

--- src/oceanwing/eufy/device/basedevice.go
+++ src/oceanwing/eufy/device/basedevice.go
@@ -2,14 +2,31 @@ package device
 
 // EufyDevice 所有 eufy device 的行为接口
 type EufyDevice interface {
+	// 当订阅的主题有消息推送下来时，用此方法来处理消息
 	HandleSubscribeMessage()
+
+	// 获取当前 Device 订阅的主题
 	GetSubTopic() string
+
+	// 获取发布给当前Device消息时使用的主题
 	GetPubTopic() string
+
+	// 获取当前 Device 的 Product Code
 	GetProductCode() string
+
+	// 获取当前 Device 的 Device Key
 	GetProductKey() string
+
+	// 当测试结束时，获取对当前Device发出的指令总数
 	GetSentCmds() int
+
+	// 当测试结束时，获取当前Device的心跳总数
 	GetDecodedheartBeat() int
+
+	// 把订阅来的消息转给指定的 Channel
 	SendPayload([]byte)
+
+	// 组装消息并序列化，用于 publish
 	BuildProtoBufMessage() []byte
 }
 
